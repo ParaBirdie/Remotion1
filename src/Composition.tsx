59fde0a41f8f6fe29a3d4ef9db762d5bd1f9ba96
ParaBirdie <sophia.pan2003@gmail.com>
2026-05-25 12:01:48 -0700

    Reorder: Section 3 first (0–8s), then Section 1 (8–15s), then Section 2 (15–22s)

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -6,14 +6,14 @@ import { Section3 } from './sections/Section3';
 export const MyComposition: React.FC = () => {
   return (
     <>
-      <Sequence from={0} durationInFrames={210}>
+      <Sequence from={0} durationInFrames={240}>
+        <Section3 />
+      </Sequence>
+      <Sequence from={240} durationInFrames={210}>
         <Section1 />
       </Sequence>
-      <Sequence from={210} durationInFrames={210}>
+      <Sequence from={450} durationInFrames={210}>
         <Section2 />
-      </Sequence>
-      <Sequence from={420} durationInFrames={240}>
-        <Section3 />
       </Sequence>
     </>
   );
